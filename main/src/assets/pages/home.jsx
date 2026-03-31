63415998226ed2ae2ba33885c4e0f8d2ed3b589e
John Mhyckel <johnmhyckeldomingo@gmail.com>
2026-03-31 12:04:24 +0800

    second commit error

---
+++ main/src/assets/pages/home.jsx
@@ -0,0 +1,20 @@
+import "./home.css";
+
+const Home = () => {
+    return (
+        <main className="mem">
+            <h1>BSIT-2A Group 8 Members</h1>
+            <p>This Group need buff</p>
+            <section>
+                <ul>
+                    <li><strong>Member 1: </strong>ABLANIA</li>
+                    <li><strong>Member 2: </strong>BATO</li>
+                    <li><strong>Member 3: </strong>VILLANUEVA</li>
+                    <li><strong>Member 4: </strong>Casaljay</li>
+                </ul>
+            </section>
+        </main>
+    );
+}
+
+export default Home;
